8681571c9176a8683ee78656f7c7215efa3926e1
szuturon <ronnyszutu@gmail.com>
2014-01-08 00:06:24 -0500

    Creating Keyword, StepAction, and Testcase classes

---
+++ TestLinkParser/Testcase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace TestLinkParser
+{
+    class Testcase
+        XmlNode node_order, externalid, version, summary, preconditions, execution_type, importance;
+    {
+    }
+}
